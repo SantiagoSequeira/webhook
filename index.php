4edb33023a6a2510c484a3b733119a3b29527097
SantiagoSequeira <santiagoezequielsequeira@gmail.com>
2022-06-11 20:09:13 -0400

    modifyCode

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <?php
-// Original Answer
-header('Content-Type: application/json');
-$request = file_get_contents('php://input');
-$req_dump = print_r( $request, true );
-$fp = file_put_contents( 'request.log', $req_dump );
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
-// Updated Answer
-if($json = json_decode(file_get_contents("php://input"), true)){
-   $data = $json;
+// Set this Verify Token Value on your Facebook App 
+if ($verify_token === 'testtoken') {
+  echo $challenge;
 }
-print_r($data);
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+// Get the Senders Graph ID
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+
+// Get the returned message
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+//API Url and Access Token, generate this token value on your Facebook App Page
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<ACCESS-TOKEN-VALUE>';
+
+//Initiate cURL.
+$ch = curl_init($url);
+
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"' . $sender . '"
+    }, 
+    "message":{
+        "text":"The message you want to return"
+    }
+}';
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+//Execute the request but first check if the message is not empty.
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+  $result = curl_exec($ch);
+}
 ?>
