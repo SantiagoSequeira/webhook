10c2b04e8b8857f51c7cd515ac2707f3c69d6d17
SantiagoSequeira <santiagoezequielsequeira@gmail.com>
2022-06-11 21:21:57 -0400

    test

--- index.php
+++ index.php
@@ -7,8 +7,40 @@ if ($verify_token === 'testtoken') {
   echo $challenge;
 }
 
-$input = json_encode(json_decode(file_get_contents('php://input'), true));
+$input = json_decode(file_get_contents('php://input'), true);
 
-error_log($input);
+error_log(json_encode($input));
+
+
+sendMessage($input->entry[0]->changes[0]->value->messages[0]->text->body)
+
+
+
+
+
+function sendMessage($message) {
+  $sessionId = '27c0a22c-a226-48aa-91b5-23a07f1b5a9f!1654996893555!8+0mYpddhM3kx+CJOrOxAjrQIDg=';
+  $affinity = '99b51fa0';
+  $url = 'https://d.la1-c2-ia4.salesforceliveagent.com/chat/rest/Chasitor/ChatMessage';
+  // use key 'http' even if you send the request to https://...
+  $options = array(
+      'http' => array(
+          'header'  => "Content-type: application/json\r\n" . 
+              "X-LIVEAGENT-API-VERSION: 50\r\n" . 
+              "X-LIVEAGENT-SESSION-KEY: $sessionId\r\n" .
+              "X-LIVEAGENT-AFFINITY: $affinity\r\n" ,
+          'method'  => 'POST',
+          'content' => '{
+              text: " ' . $message . '"
+          }'
+      )
+  );
+  $context  = stream_context_create($options);
+  $result = json_decode(file_get_contents($url, false, $context));
+  if ($result === FALSE) {
+
+  }
+  
+}
 
 ?>
--- sendMessage.php
+++ sendMessage.php
@@ -1,6 +1,6 @@
 <?php
+    
     $url = 'https://d.la1-c2-ia4.salesforceliveagent.com/chat/rest/System/SessionId';
-
     // use key 'http' even if you send the request to https://...
     $options = array(
         'http' => array(
@@ -15,4 +15,77 @@
     }
     error_log(json_encode($result));
     var_dump($result);
+
+    function iniciateChatSession($sessionId, $affinity){
+        $url = 'https://d.la1-c2-ia4.salesforceliveagent.com/chat/rest/Chasitor/ChasitorInit';
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/json\r\n" . 
+                            "X-LIVEAGENT-API-VERSION: 50\r\n" . 
+                            "X-LIVEAGENT-SESSION-KEY: $sessionKey\r\n",
+                            "X-LIVEAGENT-AFFINITY: $affinity\r\n",
+                            "X-LIVEAGENT-SEQUENCE: 1",
+                'method'  => 'POST',
+                'content' => '{
+                    "organizationId":"00D4R0000007tWz",
+                    "deploymentId":"5724R000000c2nq",
+                    "buttonId":"5734R000000c3JX",
+                    "sessionId": null,
+                    "trackingId":"",
+                    "userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+                    "language":"es-US",
+                    "screenResolution":"1366x768",
+                    "visitorName":"Santiago",
+                    "prechatDetails":[
+                    {
+                        "label":"UalaUsername",
+                        "value":"batman",
+                        "entityMaps":[
+                            {
+                                "entityName": "Account",
+                                "fieldName": "c__UalaUsername",
+                                "isFastFillable": true,
+                                "isAutoQueryable": true,
+                                "isExactMatchable": true
+                            }
+                        ],
+                        "transcriptFields":[],
+                        "displayToAgent":true
+                    },
+                    {
+                        "label":"Country",
+                        "value": "032",
+                        "entityMaps":[],
+                        "transcriptFields":[],
+                        "displayToAgent":true
+                    }
+                    ],
+                    "buttonOverrides":[
+                
+                    ],
+                    "receiveQueueUpdates":false,
+                    "prechatEntities":[],
+                    "isPost":false,
+                    "visitorInfo":{
+                    "visitCount":2,
+                    "originalReferrer":"",
+                    "pages":[
+                        {
+                            "location":"",
+                            "time":1591808266234
+                        }
+                    ]
+                    }
+                }'
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) {
+
+        }
+        var_dump($result);
+    }
+    
 ?>
